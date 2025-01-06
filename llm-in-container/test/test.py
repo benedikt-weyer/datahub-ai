f6282044b1ace280981c0f5b57e1eb3ddf1bdd96
wrp687 <jan.biedasiek@haw-hamburg.de>
2025-01-06 10:14:15 +0100

    added questions and expectations

--- llm-in-container/test/test.py
+++ llm-in-container/test/test.py
@@ -7,8 +7,8 @@ from llama_index.llms.ollama import Ollama
 #with open('test/expected_output.txt', 'r') as file:
 #    expected_output = [row.strip() for row in file]
 
-input = "For my research project on malaria, I need precipitation data for the period from January 2020 to December 2023. Are these data available, and in what resolution?"
-expected_output = "Thank you for your question. Yes, I have found precipitation data for Ghana. However, the data is only available for the period January 2021 to December 2023 in daily resolution. I can offer you two different data sources: Precipitation (satellite) (chirps_prcp) and Precipitation (station) (meteo_prcp)."
+questions = ["For my research project on malaria, I need precipitation data for the period from January 2020 to December 2023. Are these data available, and in what resolution?"]
+expected_answers = ["Thank you for your question. Yes, I have found precipitation data for Ghana. However, the data is only available for the period January 2021 to December 2023 in daily resolution. I can offer you two different data sources: Precipitation (satellite) (chirps_prcp) and Precipitation (station) (meteo_prcp)."]
 
 # create llm
 #llm_test = Ollama(base_url='http://benedikt-home-server.duckdns.org:11434', model="dolphin-mistral:latest", request_timeout=30.0)
@@ -16,13 +16,13 @@ llm_test = Ollama(base_url='http://localhost:11434', model="dolphin-llama3:lates
 
 
 # check testcases with llm
-for i in range(len(input)):
-    output = "Hello World" #submit_query(input[i])
-    print("Input: " + input[i])
-    print("expected Output: " + expected_output[i])
-    print("Output: " + output)
+for i in range(len(questions)):
+    answer = "Hello World" #submit_query(input[i])
+    print("Input: " + questions[i])
+    print("expected Output: " + expected_answers[i])
+    print("Output: " + answer)
     #vergleiche output mit expected_output
-    prompt = "Give me a rating from 1-10 how the text \"output\" matches text \"expected_output\" semantically. output = " + output + "; expected_output = " + expected_output
+    prompt = "Give me a rating from 1-10 how the text \"answer\" matches text \"expected_answer\" semantically or 0 if the texts arent correlated. output = " + answer + "; expected_output = " + expected_answers[i]
     #llm den prompt übergeben und Antwort printen
-    answer = llm_test(prompt)
-    print(answer)
+    result = llm_test.complete(prompt)
+    print(result)
