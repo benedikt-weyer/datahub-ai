b77a8dfd199b96b632490f36bed59b73badfbb56
Benedikt Weyer <105204092+benedikt-weyer@users.noreply.github.com>
2025-01-07 17:33:45 +0100

    Add endpoint to retrieve table names from the database

--- llm-in-container/llm_query_engine_api.py
+++ llm-in-container/llm_query_engine_api.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify, Response
-from advanced_sql_query_engine import submit_query
 from flask_cors import CORS
+
+from advanced_sql_query_engine import submit_query
+from data_description_access import get_table_names
 
 # Initialize the Flask application and the LLMQueryEngine instance
 app = Flask(__name__)
@@ -37,6 +39,30 @@ def query_post():
         "query": query_string,
         "response": response,
     })
+
+
+
+@app.route('/data-description', methods=['GET'])
+def query_get():
+    # Access the query parameter from the URL
+    #query_string = request.args.get("query")
+
+    #if not query_string:
+    #    return jsonify({"error": "Missing 'query' parameter"}), 400
+
+    # Create a response using the query string
+    #response = f'You just submitted the query: {query_string}'
+    table_names = get_table_names()
+
+    # Return the JSON response
+    return jsonify({
+        "table-names": table_names,
+    })
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8001)
