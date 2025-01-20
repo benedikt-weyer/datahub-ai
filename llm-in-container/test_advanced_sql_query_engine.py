e9f20e8766bb283492291155d77b4648b793ccdf
wrp687 <jan.biedasiek@haw-hamburg.de>
2025-01-20 12:46:08 +0100

    moved file

--- llm-in-container/test_advanced_sql_query_engine.py
+++ llm-in-container/test_advanced_sql_query_engine.py
@@ -1,7 +1,8 @@
 import unittest
 
+from datahub_ai.ai import advanced_sql_query_engine
 
 class TestAddOneToNr(unittest.TestCase):
     def test_add_once(self):
-        res = add_one_to_nr(1)
+        res = advanced_sql_query_engine.add_one_to_nr(1)
         self.assertEqual(2,2)
