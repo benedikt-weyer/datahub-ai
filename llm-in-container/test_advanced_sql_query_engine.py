0e5aef54c1798d7d83026f9585e72632b6bdb185
Benedikt Weyer <105204092+benedikt-weyer@users.noreply.github.com>
2025-01-21 13:29:27 +0100

    refactor unit tests for advanced SQL query engine and improve assertions

--- llm-in-container/test_advanced_sql_query_engine.py
+++ llm-in-container/test_advanced_sql_query_engine.py
@@ -5,31 +5,24 @@ from llama_cloud import ChatMessage, MessageRole
 from llama_index.core.llms import ChatResponse
 from datahub_ai.ai import advanced_sql_query_engine
 
-class TestAddOneToNr(unittest.TestCase):
+class TestAdvancedSQLQueryEngine(unittest.TestCase):
 
-    #def test_parse_response_to_sql_no_sql(self):
-    #    message = ChatMessage(
-    #        role=MessageRole.CHATBOT,
-    #        content="No SQL here",
-    #        id=str(uuid4()),  # Generiert eine eindeutige ID
-    #        index=0  # Setzen Sie den Index entsprechend
-    #    )
-    #    chat_response = ChatResponse(message={"message": message})
-    #    res = advanced_sql_query_engine.parse_response_to_sql(chat_response);
-    #    self.assertEqual("WITH non_existent_table AS (SELECT 'no sql query provided' as error) SELECT * FROM non_existent_table;", res)
+    def test_parse_respond_to_sql_basic(self):
+        response_str = "SQLQuery: WITH non_existent_table AS (SELECT 'no sql query provided' as error) SELECT * FROM non_existent_table;"
 
-    def test_parse_respond_to_sql_sql(self):
-        message = ChatMessage(
+        chat_message = ChatMessage(
             role=MessageRole.ASSISTANT,
-            content="SQLQuery: Hello World",
+            content=response_str,
             id=str(uuid4()),  # Generiert eine eindeutige ID
             index=0  # Setzen Sie den Index entsprechend
         )
-        chat_response = ChatResponse(message={"message": message})
-        print(type(chat_response))
-        print(chat_response.__dict__)
-        print(type(chat_response.message))
-        print(chat_response.message.__dict__)
+        chat_response = ChatResponse(message={"message": chat_message})
 
-        #res = advanced_sql_query_engine.parse_response_to_sql(chat_response);
-        self.assertEqual("SQLQuery:HelloWorld", "res")
+
+        sql = advanced_sql_query_engine.parse_response_to_sql(chat_response);
+
+
+        self.assertEqual(sql, "WITH non_existent_table AS (SELECT 'no sql query provided' as error) SELECT * FROM non_existent_table;")
+
+if __name__ == "__main__":
+    unittest.main()
