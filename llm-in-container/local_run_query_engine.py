c899b1c6fbae7cdeffbf6112a81f2128dc9821e3
Benedikt Weyer <105204092+benedikt-weyer@users.noreply.github.com>
2025-01-23 14:47:31 +0100

    fix: update datahub metadata retrieval to print output

--- llm-in-container/local_run_query_engine.py
+++ llm-in-container/local_run_query_engine.py
@@ -1,8 +1,8 @@
 #from advanced_sql_query_engine import submit_query
 from datahub_ai.logic.datahub_metadata_logic import get_datahub_tables_metadata
-from datahub_ai.ai.custom_rag_pipeline_ai import ai_engine
+#from datahub_ai.ai.custom_rag_pipeline_ai import ai_engine
 
-get_datahub_tables_metadata(without_docker_flag=True)
+print(get_datahub_tables_metadata(without_docker_flag=True))
 #response = submit_query("What districts are in the data?", True)
 #response = submit_query("How was the percipitation in Korle Klottey Municipal in 2020?", True)
 #response = submit_query("How was the percipitation in Korle Klottey Municipal in 2020?", True, 'http://localhost:11434')
