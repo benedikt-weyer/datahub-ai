a5f1d74d4d105d08214aca962ca8ecef083ac996
benedikt-weyer <bw.development@pm.me>
2025-03-22 17:19:47 +0100

    refactor: improve imports

--- datahub_ai/datahub_ai/api/llm_query_engine_api.py
+++ datahub_ai/datahub_ai/api/llm_query_engine_api.py
@@ -1,13 +1,15 @@
 import json
+from bson import json_util
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from bson import json_util
 
 from llama_index.core.storage.chat_store import SimpleChatStore
 
-
 from datahub_ai.logic import data_description_logic, query_logic
+
+
+
 
 # Initialize the Flask application and the LLMQueryEngine instance
 app = Flask(__name__)
@@ -21,17 +23,17 @@ def query_post():
     query_string = data.get("query")
     is_verbose = data.get("is_verbose")
     chat_store_json = data.get("chat_store_json", None)
-    
+
     chat_store = None
     if chat_store_json is not None:
         chat_store = SimpleChatStore.from_json(chat_store_json)
-    
+
     if not query_string:
         return jsonify({"error": "Missing 'query' parameter"}), 400
-    
+
     if not is_verbose:
         is_verbose = False
-    
+
     result = query_logic.query_ai(query_string, is_verbose, chat_store=chat_store)
 
     chat_store_out = result.get("chat_store", None)
@@ -50,7 +52,7 @@ def query_post():
 
 @app.route('/api/data-description/inactive-table-names', methods=['GET'])
 def data_description_inactive_table_names_get():
-    
+
     inactive_table_names = data_description_logic.get_inactive_table_names()
 
     # Return the JSON response
@@ -73,10 +75,10 @@ def data_description_active_tables_get():
 def data_description_active_tables_post():
     data = request.json
     table_name = data.get("table_name")
-    
+
     if not table_name:
         return jsonify({"error": "Missing 'table_name' parameter"}), 400
-    
+
     response = data_description_logic.add_table_without_description(table_name)
     return jsonify({
         "table_name": table_name,
@@ -87,13 +89,13 @@ def data_description_active_tables_put():
     data = request.json
     table_name = data.get("table_name")
     table_description = data.get("table_description")
-    
+
     if not table_name:
         return jsonify({"error": "Missing 'table_name' parameter"}), 400
-    
+
     if not table_description:
         return jsonify({"error": "Missing 'table_description' parameter"}), 400
-    
+
     response = data_description_logic.update_table(table_name, table_description)
 
     return jsonify({
@@ -105,10 +107,10 @@ def data_description_active_tables_put():
 def data_description_active_tables_delete():
     data = request.json
     table_name = data.get("table_name")
-    
+
     if not table_name:
         return jsonify({"error": "Missing 'table_name' parameter"}), 400
-    
+
     response = data_description_logic.remove_table(table_name)
     return jsonify({
         "table_name": table_name,
@@ -119,10 +121,10 @@ def data_description_active_tables_delete():
 def data_description_active_tables_import():
     data = request.json
     file_data = data.get("file_data")
-    
+
     if not file_data:
         return jsonify({"error": "Missing 'file_data' parameter"}), 400
-    
+
     response = data_description_logic.import_active_tables(file_data)
     return jsonify({
         "response": response,
