147f15f7ee24247be2b89f16d8ced38324250fb8
benedikt-weyer <bw.development@pm.me>
2025-04-21 18:49:30 +0200

    refactor: remove unused 'without_docker' parameter from query functions and streamline database connection handling

--- datahub_ai/datahub_ai/logic/ai_query_execution.py
+++ datahub_ai/datahub_ai/logic/ai_query_execution.py
@@ -1,4 +1,4 @@
 from datahub_ai.data_access import datahub_tables_access
 
 def execute_generated_query(sql_query, without_docker=False):
-    return datahub_tables_access.execute_generated_query(sql_query, without_docker=without_docker)
+    return datahub_tables_access.execute_generated_query(sql_query)
