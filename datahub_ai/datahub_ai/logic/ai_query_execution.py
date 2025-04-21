a575b3991985cea9ab044ee50a1831c16145920d
benedikt-weyer <bw.development@pm.me>
2025-04-21 18:50:25 +0200

    refactor: remove 'without_docker' parameter from execute_generated_query function and update its usage in submit_query

--- datahub_ai/datahub_ai/logic/ai_query_execution.py
+++ datahub_ai/datahub_ai/logic/ai_query_execution.py
@@ -1,4 +1,4 @@
 from datahub_ai.data_access import datahub_tables_access
 
-def execute_generated_query(sql_query, without_docker=False):
+def execute_generated_query(sql_query):
     return datahub_tables_access.execute_generated_query(sql_query)
